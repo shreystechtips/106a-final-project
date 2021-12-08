c31f22d4f7c90c86759ad97199cf447106f3492e
Karthik Dharmarajan <karthikdharmarajan123@gmail.com>
2021-12-08 15:33:15 -0800

    Fixed realsense config

--- Perception/main.py
+++ Perception/main.py
@@ -30,6 +30,7 @@ if __name__ == '__main__':
         pipeline_profile = config.resolve(pipeline_wrapper)
         device = pipeline_profile.get_device()
         device_product_line = str(device.get_info(rs.camera_info.product_line))
+        config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
         config.enable_stream(rs.stream.depth, 640, 480, rs.format.bgr8, 30)
 
         pipeline.start(config)
