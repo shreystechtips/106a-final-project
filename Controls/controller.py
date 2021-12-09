56a9dd3e8ac0803cfd15d81fbc96496dfea9e980
Karthik Dharmarajan <karthikdharmarajan123@gmail.com>
2021-12-08 16:14:16 -0800

    Added controller.py

---
+++ Controls/controller.py
@@ -0,0 +1,13 @@
+import numpy as np
+import serial
+
+ser = serial.Serial('/dev/ttyUSB0')
+ser.baudrate = 19200
+
+def send_to_coords(coords: np.array):
+    ''' Moves magnet mechanism to specify coordinates in "coords"
+        Input coords in a np.array of size (2,) representing x, y in millimeters in workspace frame 
+    '''
+    command = f'G01 X{coords[0]} Y{coords[1]}'
+    ser.write(bytes(command, 'utf-8'))
+
