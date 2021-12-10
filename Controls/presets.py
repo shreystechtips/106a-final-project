f23b31e54a6edc9e861a63f34b49962672c35198
Shrey A <Shreystechtips@gmail.com>
2021-12-10 02:17:39 -0800

    fix sizing

--- Controls/presets.py
+++ Controls/presets.py
@@ -5,7 +5,7 @@ def swirl_preset(rate_theta = 15/180*np.pi, rate_r = 3, steps = 50, offset = 0):
     rads = np.linspace(0, steps*rate_r, num=steps)
     x = (rads*np.cos(thetas) + offset).astype(int)
     y = (rads*np.sin(thetas) + offset).astype(int)
-    size = max(np.max(x), np.max(y))
+    size = max(np.max(x), np.max(y)) - min(np.min(x), np.min(y))
     return list(zip(x,y)), (size,size)
 
 def cardiod_preset(a = 20, steps = 50, offset = 0):
@@ -13,7 +13,7 @@ def cardiod_preset(a = 20, steps = 50, offset = 0):
     sin = np.sin(np.linspace(0, 2*np.pi, num=steps))
     x = (a*cos*(1-cos) + offset).astype(int)
     y = (a*sin*(1-cos) + offset).astype(int)
-    size = max(np.max(x), np.max(y))
+    size = max(np.max(x), np.max(y)) - min(np.min(x), np.min(y))
     import matplotlib.pyplot as plt
     plt.plot(x,y)
     plt.show()
@@ -24,8 +24,18 @@ def lisajous_preset(offset = 0, steps = 200):
     y = 3*np.sin(np.linspace(0, 8*np.pi, num=steps)) + offset
     x = x
     y = y
-    size = max(np.max(x), np.max(y))
+    size = max(np.max(x), np.max(y)) - min(np.min(x), np.min(y))
     import matplotlib.pyplot as plt
     plt.plot(x,y)
     plt.show()
     return list(zip(x,y)), (size, size)
+    
+SCALE_SIZE = 300
+def transform(point, old_dim, new_dim = SCALE_SIZE):
+    scale = new_dim/old_dim
+    old_dim = np.array(old_dim)
+    new_dim = np.array(new_dim)
+    point = np.array(point) 
+    temp =  scale*(point - old_dim/2) + new_dim/2
+    temp[0] = -temp[0] + new_dim
+    return temp
