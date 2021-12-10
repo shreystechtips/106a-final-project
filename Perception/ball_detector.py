5ea5043b0ef62183282b24321378e47a89827d27
Karthik Dharmarajan <karthikdharmarajan123@gmail.com>
2021-12-09 19:17:54 -0800

    Fixed ball_detector indentations

--- Perception/ball_detector.py
+++ Perception/ball_detector.py
@@ -61,30 +61,30 @@ def get_ball_location(frame):
         counter -= 1
 
 if __name__ == '__main__':
-        pipeline = rs.pipeline()
-        config = rs.config()
+    pipeline = rs.pipeline()
+    config = rs.config()
 
-        # Get device product line for setting a supporting resolution
-        pipeline_wrapper = rs.pipeline_wrapper(pipeline)
-        pipeline_profile = config.resolve(pipeline_wrapper)
-        device = pipeline_profile.get_device()
-        device_product_line = str(device.get_info(rs.camera_info.product_line))
-        config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-        config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+    # Get device product line for setting a supporting resolution
+    pipeline_wrapper = rs.pipeline_wrapper(pipeline)
+    pipeline_profile = config.resolve(pipeline_wrapper)
+    device = pipeline_profile.get_device()
+    device_product_line = str(device.get_info(rs.camera_info.product_line))
+    config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 
-        pipeline.start(config)
-        while True:
-            frames = pipeline.wait_for_frames()
-            depth_frame = frames.get_depth_frame()
-            color_frame = frames.get_color_frame()
+    pipeline.start(config)
+    while True:
+        frames = pipeline.wait_for_frames()
+        depth_frame = frames.get_depth_frame()
+        color_frame = frames.get_color_frame()
 
-            # Convert images to numpy arrays
-            depth_image = np.asanyarray(depth_frame.get_data())
-            color_image = np.asanyarray(color_frame.get_data())
+        # Convert images to numpy arrays
+        depth_image = np.asanyarray(depth_frame.get_data())
+        color_image = np.asanyarray(color_frame.get_data())
 
-            cv.imshow('frame',color_image)
-            get_ball_location(color_image)
+        cv.imshow('frame',color_image)
+        get_ball_location(color_image)
 
-            k = cv.waitKey(1) & 0xFF
-            if k == 27:
-                break
+        k = cv.waitKey(1) & 0xFF
+        if k == 27:
+            break
